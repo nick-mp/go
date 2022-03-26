f67037f7d3d1f7e0db94df9c4f501851fca4cbf6
nick-mp <nick263mp@gmail.com>
2022-03-26 13:14:51 +0300

    clear

--- main.go
+++ main.go
@@ -1,50 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"html/template"
-)
-
-type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
-}
-
-func (u User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and"+
-		" he has money equal: %d", u.name, u.age, u.money)
-}
-
-func (u *User) setNewName(newName string) {
-	u.name = newName
-}
-
-func home_page(page http.ResponseWriter, r *http.Request) {
-	Bob := User{"Bob", 25, -50, 4.2, 0.8}
-	//Bob.setNewName("Alex")
-	//fmt.Fprintf(page, Bob.getAllInfo())
-	tmpl, _ := template.ParseFiles("templates/home_page.html")
-	tmpl.Execute (page, Bob)
-}
-
-
-
-func contacts_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "contacts page!")
-}
-
-func handleRequest() {
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/contacts/", contacts_page)
-	http.ListenAndServe(":8080", nil)
-}
+import "fmt"
 
 func main() {
-	// var Bob user =
-	//Bob := User{name: "Bob", age: 25, money: -50, avg_grades: 4.2, happiness: 0.8}
-
-	handleRequest()
+	fmt.Println("Hello World")
 }
